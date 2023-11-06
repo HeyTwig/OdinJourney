a9a936d31cf7124699bf6e04ef0041260a2695eb
William Gao <93678662+HeyTwig@users.noreply.github.com>
2023-11-05 22:56:35 -0500

    Add Variables Tasks and tests

---
+++ JavaScript Fundamentals/Part 1/javascript.js
@@ -0,0 +1,20 @@
+let hello = "World";
+alert(hello)
+console.log(hello)
+
+const Birthday_Date = "Sep 16 2004";
+let birthday = Birthday_Date;
+alert(birthday);
+
+//Variable Tasks
+let name = 'John';
+let admin = name;
+alert(admin);
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'William';
+
+const BIRTHDAY = '16.09.2004';
+//Hard-coded, we knew it prior to execution
+const age = someCode(BIRTHDAY);
+//even though it doesn't change through code execution, the value changes based on the year you ran the code. It is evaluated in run-time, during the execution.
