828938460d796ad13a3aa4521a6556a575d51cf9
William Gao <93678662+HeyTwig@users.noreply.github.com>
2023-11-23 00:04:26 -0500

    Add logical operation section

--- JavaScript Fundamentals/Part 2/script.js
+++ JavaScript Fundamentals/Part 2/script.js
@@ -53,3 +53,15 @@ undefined == null // true
 undefined === null //false
 null == "\n0\n" // false
 null === +"\n0\n" //false
+
+
+//Logical operators
+alert( null || 2 || undefined ); // 2
+alert( alert(1) || 2 || alert(3) ); //1, 2
+alert( 1 && null && 2 ); //null
+alert( alert(1) && alert(2) );//1, undefine
+if (age >= 14 && age <= 90)
+if (!(age >= 14 && age <= 90))
+if (-1 || 0) alert( 'first' ); //first
+if (-1 && 0) alert( 'second' ); //
+if (null || -1 && 1) alert( 'third' ); //third
