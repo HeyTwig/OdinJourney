1e7b66669ff28e2ea165b0ee3d1e72acadfb5d89
Will <WilliamGao2004@gmail.com>
2023-12-11 21:50:16 -0500

    Complete switch.js

---
+++ JavaScript Fundamentals/Part 2/switch.js
@@ -0,0 +1,92 @@
+The "switch" statement
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too big' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
+
+If there is no break then the execution continues with the next case without any checks.
+
+
+
+Tasks
+Rewrite the "switch" into an "if"
+importance: 5
+
+Write the code using if..else which would correspond to the following switch:
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+if (browser = 'Edge') {
+  alert( "Youve got the Edge!)
+} else if (browser = 'Chrome'
+  || browser = 'Firefox'
+  || browser = 'Safari'
+  || browser = 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
+}
+
+
+
+
+Rewrite "if" into "switch"
+importance: 4
+
+Rewrite the code below using a single switch statement:
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
+
+let a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+
+  case 1:
+    alert(1);
+    break;
+
+  case 2:
+  case 3:
+    alert('2,3');
+    break;
+}
