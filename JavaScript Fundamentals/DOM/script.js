8abd2122f93a7e1b441b87944c644e34884613bd
Will <WilliamGao2004@gmail.com>
2024-01-30 19:49:26 -0500

    Create inner container with elements inside
    I create a div container inside the previous container, that has a black border with a pink background. It has a header and a paragraph inside.

--- JavaScript Fundamentals/DOM/script.js
+++ JavaScript Fundamentals/DOM/script.js
@@ -11,3 +11,25 @@ redPara.textContent = "Hey I'm red!";
 redPara.style.color = "red";
 
 container.appendChild(redPara);
+
+const blueHeader = document.createElement('h3');
+blueHeader.textContent = "I'm a blue h3!";
+blueHeader.style.color = "blue";
+
+container.appendChild(blueHeader);
+
+const innerContainer = document.createElement("div");
+innerContainer.classList.add('innerContainer');
+innerContainer.style.cssText = "border: solid black; background-color:pink;";
+
+container.appendChild(innerContainer);
+
+const innerHeader = document.createElement("h1");
+innerHeader.textContent = "I'm in a div";
+
+innerContainer.appendChild(innerHeader);
+
+const innerPara = document.createElement("p");
+innerPara.textContent = "ME TOO!"
+
+innerContainer.appendChild(innerPara)
