16a5452ddb189ee800bebae7118ade1bc069a953
Will <WilliamGao2004@gmail.com>
2024-01-30 19:32:39 -0500

    Add paragraph with red text
    I added a new paragraph to index.html, using javascript for the first time

---
+++ JavaScript Fundamentals/DOM/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const redPara = document.createElement('p');
+redPara.textContent = "Hey I'm red!";
+redPara.style.color = "red";
+
+container.appendChild(redPara);
