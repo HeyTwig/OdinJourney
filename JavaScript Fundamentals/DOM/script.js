6cb864e048bc2a9df8691c65c89193f6a8fe9f25
Will <WilliamGao2004@gmail.com>
2024-02-20 14:36:17 -0500

    Testing the examples

--- JavaScript Fundamentals/DOM/script.js
+++ JavaScript Fundamentals/DOM/script.js
@@ -1,25 +1,25 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
 
-const redPara = document.createElement('p');
+const redPara = document.createElement("p");
 redPara.textContent = "Hey I'm red!";
 redPara.style.color = "red";
 
 container.appendChild(redPara);
 
-const blueHeader = document.createElement('h3');
+const blueHeader = document.createElement("h3");
 blueHeader.textContent = "I'm a blue h3!";
 blueHeader.style.color = "blue";
 
 container.appendChild(blueHeader);
 
 const innerContainer = document.createElement("div");
-innerContainer.classList.add('innerContainer');
+innerContainer.classList.add("innerContainer");
 innerContainer.style.cssText = "border: solid black; background-color:pink;";
 
 container.appendChild(innerContainer);
@@ -30,6 +30,18 @@ innerHeader.textContent = "I'm in a div";
 innerContainer.appendChild(innerHeader);
 
 const innerPara = document.createElement("p");
-innerPara.textContent = "ME TOO!"
+innerPara.textContent = "ME TOO!";
 
-innerContainer.appendChild(innerPara)
+innerContainer.appendChild(innerPara);
+
+// const btn = document.querySelector('#btn');
+// btn.onclick = () => alert("Hello World");
+
+const btn = document.querySelector("#btn");
+// btn.addEventListener('click', () => {
+//   alert("Hello World");
+// });
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+});
