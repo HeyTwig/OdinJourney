3362b6f010aae576f8ec52b9c5ccb71edc2d819e
Will <WilliamGao2004@gmail.com>
2024-02-27 20:01:43 -0500

    Completed MDN Challenge

---
+++ JavaScript Fundamentals/DOM/hw1.js
@@ -0,0 +1,24 @@
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  const myItem = input.value;
+  input.value = "";
+
+  const listItem = document.createElement("li");
+  const listTxt = document.createElement("span");
+  const listBtn = document.createElement("button");
+
+  listItem.appendChild(listTxt);
+  listTxt.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = "Delete";
+  list.appendChild(listItem);
+
+  listBtn.addEventListener("click", () => {
+    list.removeChild(listItem);
+  });
+
+  input.focus();
+});
